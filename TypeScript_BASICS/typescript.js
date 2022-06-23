a2aef3102073ca0c3f441bc678242ac13d22c09b
Ctnn <yigitcetinkaya2002@hotmail.com>
2022-06-23 14:55:20 +0300

    Start of TypeScript

---
+++ TypeScript_BASICS/typescript.js
@@ -0,0 +1,9 @@
+var a;
+a = "Bu Bir Stringtir";
+var b;
+b = 32, 123;
+var lengs;
+lengs = ["php", "Java"];
+var number;
+console.log(a, typeof (b));
+console.log.apply(console, lengs); //Bu sayede içindeki elemanların görünüşü gözükür.
---
+++ TypeScript_BASICS/typescript.ts
@@ -0,0 +1,19 @@
+let a: string;
+
+a="Bu Bir Stringtir";
+
+let b: any;
+
+b=32,123;
+
+let lengs: string[];
+
+lengs=["php","Java"]
+
+let number:number[];
+
+
+
+console.log(a,typeof(b));
+
+console.log(...lengs); //Bu sayede içindeki elemanların görünüşü gözükür.
